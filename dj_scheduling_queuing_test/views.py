c116ce12bc2b4295c1f0baa80d9c19dd9ccbd1bb
Alex Clark <aclark@aclark.net>
2014-07-23 19:19:11 -0400

    Move app code to app

--- dj_scheduling_queuing_test/views.py
+++ dj_scheduling_queuing_test/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from rq import Queue
+from worker import conn
+from utils import count_words_at_url
 
 
 def root(request):
     """
     """
+
+    q = Queue(connection=conn)
+    result = q.enqueue(count_words_at_url, 'http://heroku.com')
+
     return HttpResponse()
