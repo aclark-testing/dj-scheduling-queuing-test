3141be1d4b599bf4c23170053630b2f72d32d536
Alex Clark <aclark@aclark.net>
2014-07-23 19:27:17 -0400

    Add file(s)

--- dj_scheduling_queuing_test/views.py
+++ dj_scheduling_queuing_test/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from rq import Queue
-from worker import conn
-from utils import count_words_at_url
+from dj_scheduling_queuing_test.worker import conn
+from dj_scheduling_queuing_test.utils import count_words_at_url
 
 
 def root(request):
