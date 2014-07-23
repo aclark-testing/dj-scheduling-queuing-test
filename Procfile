22692b3370c9d4ef4f007c38c99587ce750f1246
Alex Clark <aclark@aclark.net>
2014-07-23 18:12:06 -0400

    Add file(s)

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 worker: python worker.py
+web: django-admin.py runserver 0.0.0.0:$PORT --settings=dj_scheduling_queuing_test.settings
---
+++ dj_scheduling_queuing_test/settings.py
@@ -0,0 +1,27 @@
+ALLOWED_HOSTS = [
+    '*',
+]
+
+# settings.DATABASES is improperly configured. Please supply the ENGINE value. Check settings documentation for more details.
+DATABASES = {
+    # You must define a 'default' database
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'djangossltest.db',
+    }
+}
+
+# You must set settings.ALLOWED_HOSTS if DEBUG is False.
+#DEBUG = True
+
+INSTALLED_APPS = (
+)
+
+MIDDLEWARE_CLASSES = (
+)
+
+# 'Settings' object has no attribute 'ROOT_URLCONF'
+ROOT_URLCONF = 'dj_scheduling_testing_test.urls'
+
+# The SECRET_KEY setting must not be empty.
+SECRET_KEY='ZU_hds)oM$%cn$Z8%88x%97Wjx2;8`'
